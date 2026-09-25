47c4b093ecb2138864bbbab5ed6459563b7f7c02
annathekarategirl <annathekarategirl@gmail.com>
2026-09-25 17:08:00 -0400

    fs

---
+++ Lockers.java
@@ -0,0 +1,12 @@
+/*
+Anna Moore
+9/25/2026
+Lockers.java
+Puzzle involving opening and closing lockers. Perfect squares should be the remaining open lockers
+*/
+
+public class Lockers{
+    public static main void(String[] args){
+
+    }
+}
